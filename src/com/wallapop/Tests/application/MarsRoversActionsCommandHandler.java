5025231388ec2ad936445795ab3267f2ade30fe2
farmijo <farmijo16@gmail.com>
2021-04-18 15:34:56 +0200

    Adding factory on integration Test

--- src/com/wallapop/Tests/application/MarsRoversActionsCommandHandler.java
+++ src/com/wallapop/Tests/application/MarsRoversActionsCommandHandler.java
@@ -1,74 +1,76 @@
-//package com.wallapop.Tests.application;
-//
-//import com.wallapop.marsRover.application.builders.SessionCreationData;
-//import com.wallapop.marsRover.application.commands.*;
-//import com.wallapop.marsRover.application.handlers.CreateSessionCommandHandler;
-//import com.wallapop.marsRover.application.handlers.MarsRoverActionCommandHandler;
-//import com.wallapop.marsRover.application.services.MarsRoverService;
-//import com.wallapop.marsRover.domain.*;
-//import com.wallapop.marsRover.infrastructure.InMemoryMarsRoverRepository;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//
-//import java.util.ArrayList;
-//
-//public class MarsRoversActionsCommandHandler {
-//    MarsRoverActionCommandHandler handler;
-//    InMemoryMarsRoverRepository repository;
-//    MarsRoverService service;
-//
-//    @Before
-//    public void ActionsCommandHandler() {
-//        repository = new InMemoryMarsRoverRepository();
-//        service = new MarsRoverService(repository);
-//        handler = new MarsRoverActionCommandHandler(service);
-//
-//        var initialOrientation = Orientation.EAST;
-//        var map = new Field.Builder(4, 4).build();
-//        var initialCoordinates = new CoordinateSystem(1, 1, map);
-//        var marsRover = new MarsRover(initialCoordinates, initialOrientation);
-//
-//        this.repository.saveMarsRover(marsRover);
-//    }
-//
-//    @Test
-//    public void moveLeft_isCalled() throws Exception {
-//        var command = new MarsRoverRotationLeftCommand();
-//        this.handler.dispatch(command);
-//
-//        var marsRover = repository.getMarsRover();
-//
-//        Assert.assertEquals(marsRover.getOrientation(), Orientation.NORTH);
-//    }
-//
-//    @Test
-//    public void moveRight_isCalled() throws Exception {
-//        var command = new MarsRoverRotationRightCommand();
-//        this.handler.dispatch(command);
-//
-//        var marsRover = repository.getMarsRover();
-//
-//        Assert.assertEquals(marsRover.getOrientation(), Orientation.SOUTH);
-//    }
-//    @Test
-//    public void moveForward_isCalled() throws Exception {
-//        var command = new MarsRoverMoveForwardCommand();
-//        this.handler.dispatch(command);
-//
-//        var marsRover = repository.getMarsRover();
-//
-//        Assert.assertEquals(marsRover.getRoverX(), 2);
-//    }
-//    @Test
-//    public void moveBackwards_isCalled() throws Exception {
-//        var command = new MarsRoverMoveBackwardCommand();
-//        this.handler.dispatch(command);
-//
-//        var marsRover = repository.getMarsRover();
-//
-//        Assert.assertEquals(marsRover.getRoverX(), 0);
-//    }
-//
-//}
+package com.wallapop.Tests.application;
+
+import com.wallapop.Tests.domain.MarsRoverFactory;
+import com.wallapop.marsRover.application.builders.SessionCreationData;
+import com.wallapop.marsRover.application.commands.*;
+import com.wallapop.marsRover.application.handlers.CreateSessionCommandHandler;
+import com.wallapop.marsRover.application.handlers.MarsRoverActionCommandHandler;
+import com.wallapop.marsRover.application.services.MarsRoverService;
+import com.wallapop.marsRover.domain.*;
+import com.wallapop.marsRover.infrastructure.InMemoryMarsRoverRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+
+public class MarsRoversActionsCommandHandler {
+    MarsRoverActionCommandHandler handler;
+    InMemoryMarsRoverRepository repository;
+    MarsRoverService service;
+
+    @Before
+    public void ActionsCommandHandler() {
+        repository = new InMemoryMarsRoverRepository();
+        service = new MarsRoverService(repository);
+        handler = new MarsRoverActionCommandHandler(service);
+
+        var initialOrientation = Orientation.EAST;
+        var sizeField = new Coordinate(4,4);
+        var initialCoordinate = new Coordinate(1,1);
+        var marsRover = MarsRoverFactory.build(initialOrientation,sizeField, initialCoordinate);
+
+
+        this.repository.saveMarsRover(marsRover);
+    }
+
+    @Test
+    public void moveLeft_isCalled() throws Exception {
+        var command = new MarsRoverRotationLeftCommand();
+        this.handler.dispatch(command);
+
+        var marsRover = repository.getMarsRover();
+
+        Assert.assertEquals(marsRover.getOrientation(), Orientation.NORTH);
+    }
+
+    @Test
+    public void moveRight_isCalled() throws Exception {
+        var command = new MarsRoverRotationRightCommand();
+        this.handler.dispatch(command);
+
+        var marsRover = repository.getMarsRover();
+
+        Assert.assertEquals(marsRover.getOrientation(), Orientation.SOUTH);
+    }
+    @Test
+    public void moveForward_isCalled() throws Exception {
+        var command = new MarsRoverMoveForwardCommand();
+        this.handler.dispatch(command);
+
+        var marsRover = repository.getMarsRover();
+
+        Assert.assertEquals(marsRover.getRoverX(), 2);
+    }
+    @Test
+    public void moveBackwards_isCalled() throws Exception {
+        var command = new MarsRoverMoveBackwardCommand();
+        this.handler.dispatch(command);
+
+        var marsRover = repository.getMarsRover();
+
+        Assert.assertEquals(marsRover.getRoverX(), 0);
+    }
+
+}
--- src/com/wallapop/Tests/application/TestCreateSessionCommandHandler.java
+++ src/com/wallapop/Tests/application/TestCreateSessionCommandHandler.java
@@ -1,51 +1,51 @@
-//package com.wallapop.Tests.application;
-//
-//import com.wallapop.marsRover.application.builders.SessionCreationData;
-//import com.wallapop.marsRover.application.commands.CreateMarsRoverSessionCommand;
-//import com.wallapop.marsRover.application.commands.CreateMarsRoverSessionWithObstaclesCommand;
-//import com.wallapop.marsRover.application.handlers.CreateSessionCommandHandler;
-//import com.wallapop.marsRover.application.handlers.MarsRoverActionCommandHandler;
-//import com.wallapop.marsRover.application.services.MarsRoverService;
-//import com.wallapop.marsRover.domain.Coordinate;
-//import com.wallapop.marsRover.domain.Field;
-//import com.wallapop.marsRover.domain.MarsRoverRepository;
-//import com.wallapop.marsRover.domain.Orientation;
-//import com.wallapop.marsRover.infrastructure.InMemoryMarsRoverRepository;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//
-//import java.util.ArrayList;
-//
-//public class TestCreateSessionCommandHandler {
-//    CreateSessionCommandHandler handler;
-//    InMemoryMarsRoverRepository repository;
-//    MarsRoverService service;
-//
-//
-//    @Before
-//    public void createCommandHandler() {
-//        repository = new InMemoryMarsRoverRepository();
-//        service = new MarsRoverService(repository);
-//        handler = new CreateSessionCommandHandler(service);
-//    }
-//
-//    @Test
-//    public void createMarsRover_isCalled() throws Exception {
-//        var command = new CreateMarsRoverSessionCommand(4, 4, "e", 1, 1);
-//        this.handler.dispatch(command);
-//
-//        var marsRover = repository.getMarsRover();
-//
-//        Assert.assertEquals(marsRover.getRoverX(), 1);
-//        Assert.assertEquals(marsRover.getRoverY(), 1);
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void createMarsRoverWithObstacles_withInitialPositionOnObstacle_isCalledAndFails() throws Exception {
-//        var command = new CreateMarsRoverSessionWithObstaclesCommand(4, 4, "e", 1, 2);
-//        this.handler.dispatch(command);
-//    }
-//
-//}
+package com.wallapop.Tests.application;
+
+import com.wallapop.marsRover.application.builders.SessionCreationData;
+import com.wallapop.marsRover.application.commands.CreateMarsRoverSessionCommand;
+import com.wallapop.marsRover.application.commands.CreateMarsRoverSessionWithObstaclesCommand;
+import com.wallapop.marsRover.application.handlers.CreateSessionCommandHandler;
+import com.wallapop.marsRover.application.handlers.MarsRoverActionCommandHandler;
+import com.wallapop.marsRover.application.services.MarsRoverService;
+import com.wallapop.marsRover.domain.Coordinate;
+import com.wallapop.marsRover.domain.Field;
+import com.wallapop.marsRover.domain.MarsRoverRepository;
+import com.wallapop.marsRover.domain.Orientation;
+import com.wallapop.marsRover.infrastructure.InMemoryMarsRoverRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+
+public class TestCreateSessionCommandHandler {
+    CreateSessionCommandHandler handler;
+    InMemoryMarsRoverRepository repository;
+    MarsRoverService service;
+
+
+    @Before
+    public void createCommandHandler() {
+        repository = new InMemoryMarsRoverRepository();
+        service = new MarsRoverService(repository);
+        handler = new CreateSessionCommandHandler(service);
+    }
+
+    @Test
+    public void createMarsRover_isCalled() throws Exception {
+        var command = new CreateMarsRoverSessionCommand(4, 4, "e", 1, 1);
+        this.handler.dispatch(command);
+
+        var marsRover = repository.getMarsRover();
+
+        Assert.assertEquals(marsRover.getRoverX(), 1);
+        Assert.assertEquals(marsRover.getRoverY(), 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createMarsRoverWithObstacles_withInitialPositionOnObstacle_isCalledAndFails() throws Exception {
+        var command = new CreateMarsRoverSessionWithObstaclesCommand(4, 4, "e", 1, 2);
+        this.handler.dispatch(command);
+    }
+
+}
